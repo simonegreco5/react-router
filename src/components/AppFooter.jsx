5451110170a1516a28f8dd8d617f563e33318177
simonegreco5 <simoxey.greco@gmail.com>
2026-05-17 21:15:59 +0200

    - add fix in AppFooter - add main in HomePage

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,11 +1,11 @@
 export default function AppFooter(){
 
     return (
-        <footer className="bg-body-tertiary">
+        <footer className="">
             <div>
-                <h4>INFO</h4>
+                <h4><i class="bi bi-info-circle"></i> INFO</h4>
                 <ul>
-                    <li><a href="">Boolean Flight</a></li>
+                    <li><a href="">Boolean Shopping</a></li>
                     <li><a href="">Contact Us</a></li>
                     <li><a href="">Privacy Policy</a></li>
                     <li><a href="">Terms of Service</a></li>
@@ -13,17 +13,17 @@ export default function AppFooter(){
             </div>
 
             <div>
-                <h4>CUSTOMER SUPPORT</h4>
+                <h4><i class="bi bi-person-check"></i> CUSTOMER SUPPORT</h4>
                 <ul>
                     <li><a href="">booleansupport@boolean.com</a></li>
-                    <li><a href="">Mon-Sat 8.00AM-6.00PM</a></li>
+                    <li><a href="">Mon/Sat 9.00AM-6.00PM</a></li>
                     <li><a href="">Questions with Rufus</a></li>
                     <li><a href="">Cancellations & Refunds</a></li>
                 </ul>
             </div>
 
             <div>
-                <h4>PAYMENTS</h4>
+                <h4><i class="bi bi-credit-card-2-back"></i> PAYMENTS</h4>
                 <ul>
                     <li><a href="">Visa, Mastercard, Amex</a></li>
                     <li><a href="">Klarna, pay in installments</a></li>
