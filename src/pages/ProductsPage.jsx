ca55473cc62875487f16f3b4d093721c68c44026
simonegreco5 <simoxey.greco@gmail.com>
2026-05-18 21:16:19 +0200

    - add fix

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -7,15 +7,16 @@ export default function ProductsPage() {
     const api_products = 'https://fakestoreapi.com/products'
 
     const [productsData, setProductsData] = useState([]) // questo state rimane solo per l'API (questo rimane intatto)
-    const [filteredProduct, setFilteredProduct] = useState ([]) // secondo state per i prodotti filtrati (filtriamo solo questo)
+    const [filteredProduct, setFilteredProduct] = useState([]) // secondo state per i prodotti filtrati (filtriamo solo questo)
     const [filter, setFilter] = useState("")
+    const [count, setCount] = useState(0) // contatore carrello z-index
 
     function fetchData(url) {
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 setProductsData(data)
-                setFilteredProduct(data) 
+                setFilteredProduct(data)
             })
     }
 
@@ -27,62 +28,72 @@ export default function ProductsPage() {
     useEffect(() => {
 
         if (filter) {
-           const result = productsData.filter((product) => (product.category.toLowerCase() === filter.toLowerCase()))
+            const result = productsData.filter((product) => (product.category.toLowerCase() === filter.toLowerCase()))
 
-           setFilteredProduct(result)
+            setFilteredProduct(result)
 
         } else {  // invece, quando filter === ""(ovvero all), if(filter) diventa false,
             setFilteredProduct(productsData) // quindi mostriamo i dati originali dell'API (tutti i prodotti)
         }
 
     }, [filter, productsData]) // aggiungiamo productsData come dipendeza, cosi il filtro si aggiorna anche quando arrivano
-                               // nuovi dati dall'API
+    // nuovi dati dall'API
 
     return (
-        <main>
+        <main id="products_main">
 
-            {/* filtraggio dei prodotti */}
-            <h5>choose by filter</h5>
-            <select onChange={(e) => setFilter(e.target.value)} className="form-select">
-                <option value="">All</option>
-                <option value="men's clothing">Men's clothing</option>
-                <option value="women's clothing">Women's clothing</option>
-                <option value="jewelery">Jewelery</option>
-                <option value="electronics">Electronics</option>
-            </select>
+            {/* carrello dinamico in pagina - con z-index */}
+            <button className="dinamic-cart">
+                <i class="bi-basket"></i>
+                <span>{count}</span>
+            </button>
 
-            <div className="row g-5-p-5">
-                {
-                    filteredProduct?.map((item) => (
+            <div className="container-md">
 
-                        // CARD PRODUCT
-                        <div className="col-12 col-sm-6 col-md-4 g-5" key={item.id}>
-                            <div className="card shadow h-100" >
+                {/* filtraggio dei prodotti */}
+                <h5>choose by filter</h5>
+                <select onChange={(e) => setFilter(e.target.value)} className="form-select">
+                    <option value="">All</option>
+                    <option value="men's clothing">Men's clothing</option>
+                    <option value="women's clothing">Women's clothing</option>
+                    <option value="jewelery">Jewelery</option>
+                    <option value="electronics">Electronics</option>
+                </select>
 
-                                {/* BEST SELLER ? */}
-                                {(item.rating.rate >= 4 && item.rating.count >= 300) && <span className="badge">BestSeller</span>}
+                <div className="row g-5-p-5">
+                    {
+                        filteredProduct?.map((item) => (
 
-                                {/* IMAGE PRODUCT */}
-                                <img src={item.image} className="card-img-top" alt="product_image" />
+                            // CARD PRODUCT
+                            <div className="col-12 col-sm-6 col-md-4 g-5" key={item.id}>
+                                <div className="card shadow h-100" >
 
-                                {/* RATES AND ORDERS */}
-                                <div className="d-flex">
-                                    <span>{item.rating.rate >= 4 ? <i className="bi bi-star-fill"></i> : <i className="bi bi-star-half"></i>} {item.rating.rate}</span>
-                                    <span><i className="bi bi-basket"></i> {item.rating.count}</span>
-                                </div>
+                                    {/* BEST SELLER ? */}
+                                    {(item.rating.rate >= 4 && item.rating.count >= 300) && <span className="badge">BestSeller</span>}
 
-                                {/* CARD BODY */}
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">{item.title}</h5>
-                                    <h6>{item.price}$</h6>
-                                    <p className="card-text">{item.description}</p>
-                                    <button className="btn btn-primary mt-auto"><i className="bi bi-cart-check"> Buy Now</i></button>
+                                    {/* IMAGE PRODUCT */}
+                                    <img src={item.image} className="card-img-top" alt="product_image" />
+
+                                    {/* RATES AND ORDERS */}
+                                    <div className="d-flex">
+                                        <span>{item.rating.rate >= 4 ? <i className="bi bi-star-fill"></i> : <i className="bi bi-star-half"></i>} {item.rating.rate}</span>
+                                        <span><i className="bi bi-basket"></i> {item.rating.count}</span>
+                                    </div>
+
+                                    {/* CARD BODY */}
+                                    <div className="card-body d-flex flex-column">
+                                        <h5 className="card-title">{item.title}</h5>
+                                        <h6>{item.price}$</h6>
+                                        <p className="card-text">{item.description}</p>
+                                        <button onClick={() => setCount(count + 1)} className="btn btn-primary mt-auto"><i className="bi bi-cart-check"> Add to Cart</i></button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
+
         </main>
     )
 }
