cd41bd177650d41337556c31f5568697b37544a1
simonegreco5 <simoxey.greco@gmail.com>
2026-05-20 21:10:07 +0200

    - add Loader component integrate loading state in ProductsPage

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+export default function Loader() {
+
+    return (
+        <main className="min-vh-100 text-center">
+            <div className="spinner-border my-5" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </main>
+    )
+}
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,7 @@
 // sezione import 
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import Loader from "../components/Loader"
 
 export default function ProductsPage() {
 
@@ -11,13 +12,18 @@ export default function ProductsPage() {
     const [filteredProduct, setFilteredProduct] = useState([]) // secondo state per i prodotti filtrati (filtriamo solo questo)
     const [filter, setFilter] = useState("")
     const [count, setCount] = useState(0) // contatore carrello z-index
+    const [isDataLoading, setIsDataLoading] = useState(false)
 
     function fetchData(url) {
+
+        setIsDataLoading(true)
+
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 setProductsData(data)
                 setFilteredProduct(data)
+                setIsDataLoading(false)
             })
     }
 
@@ -41,68 +47,73 @@ export default function ProductsPage() {
     // nuovi dati dall'API
 
     return (
-        <main id="products_main">
+        <>
+            {isDataLoading ? <Loader /> :
 
-            {/* carrello dinamico in pagina - con z-index */}
-            <button className="dinamic-cart">
-                <i className="bi-basket"></i>
-                <span>{count}</span>
-            </button>
+                <main id="products_main">
 
-            <div className="container-md">
+                    {/* carrello dinamico in pagina - con z-index */}
+                    <button className="dinamic-cart">
+                        <i className="bi-basket"></i>
+                        <span>{count}</span>
+                    </button>
 
-                {/* filtraggio dei prodotti */}
-                <h5>choose by filter</h5>
-                <select onChange={(e) => setFilter(e.target.value)} className="form-select">
-                    <option value="">All</option>
-                    <option value="men's clothing">Men's clothing</option>
-                    <option value="women's clothing">Women's clothing</option>
-                    <option value="jewelery">Jewelery</option>
-                    <option value="electronics">Electronics</option>
-                </select>
+                    <div className="container-md">
 
-                {/* rendiamo l'intera card cliccabile per vedere in pagina il singolo prodotto */}
+                        {/* filtraggio dei prodotti */}
+                        <h5>choose by filter</h5>
+                        <select onChange={(e) => setFilter(e.target.value)} className="form-select">
+                            <option value="">All</option>
+                            <option value="men's clothing">Men's clothing</option>
+                            <option value="women's clothing">Women's clothing</option>
+                            <option value="jewelery">Jewelery</option>
+                            <option value="electronics">Electronics</option>
+                        </select>
 
-                <div className="row g-5-p-5">
+                        {/* rendiamo l'intera card cliccabile per vedere in pagina il singolo prodotto */}
 
-                    {
-                        filteredProduct?.map((item) => (
+                        <div className="row g-5-p-5">
 
-                            // CARD PRODUCT
-                            <div className="col-12 col-sm-6 col-md-4 g-5" key={item.id}>
-                                <Link to={`/product/${item.id}`} className="text-decoration-none">
+                            {
+                                filteredProduct?.map((item) => (
 
-                                    <div className="card shadow h-100" >
+                                    // CARD PRODUCT
+                                    <div className="col-12 col-sm-6 col-md-4 g-5" key={item.id}>
+                                        <Link to={`/product/${item.id}`} className="text-decoration-none">
 
-                                        {/* BEST SELLER ? */}
-                                        {(item.rating.rate >= 4 && item.rating.count >= 300) && <span className="badge">BestSeller</span>}
+                                            <div className="card shadow h-100" >
 
-                                        {/* IMAGE PRODUCT */}
-                                        <img src={item.image} className="card-img-top" alt="product_image" />
+                                                {/* BEST SELLER ? */}
+                                                {(item.rating.rate >= 4 && item.rating.count >= 300) && <span className="badge">BestSeller</span>}
 
-                                        {/* RATES AND ORDERS */}
-                                        <div className="d-flex">
-                                            <span>{item.rating.rate >= 4 ? <i className="bi bi-star-fill"></i> : <i className="bi bi-star-half"></i>} {item.rating.rate}</span>
-                                            <span><i className="bi bi-basket"></i> {item.rating.count}</span>
-                                        </div>
+                                                {/* IMAGE PRODUCT */}
+                                                <img src={item.image} className="card-img-top" alt="product_image" />
 
-                                        {/* CARD BODY */}
-                                        <div className="card-body d-flex flex-column">
-                                            <h5 className="card-title">{item.title}</h5>
-                                            <h6>{item.price}$</h6>
-                                            <p className="card-text">{item.description}</p>
-                                            <button onClick={() => setCount(count + 1)} className="btn btn-primary mt-auto"><i className="bi bi-cart-check"> Add to Cart</i></button>
-                                            {/* <Link to={`/product/${item.id}`} className="button-view"><i className="bi bi-chevron-bar-down"></i> View More</Link> */}
-                                        </div>
+                                                {/* RATES AND ORDERS */}
+                                                <div className="d-flex">
+                                                    <span>{item.rating.rate >= 4 ? <i className="bi bi-star-fill"></i> : <i className="bi bi-star-half"></i>} {item.rating.rate}</span>
+                                                    <span><i className="bi bi-basket"></i> {item.rating.count}</span>
+                                                </div>
+
+                                                {/* CARD BODY */}
+                                                <div className="card-body d-flex flex-column">
+                                                    <h5 className="card-title">{item.title}</h5>
+                                                    <h6>{item.price}$</h6>
+                                                    <p className="card-text">{item.description}</p>
+                                                    <button onClick={() => setCount(count + 1)} className="btn btn-primary mt-auto"><i className="bi bi-cart-check"> Add to Cart</i></button>
+                                                    {/* <Link to={`/product/${item.id}`} className="button-view"><i className="bi bi-chevron-bar-down"></i> View More</Link> */}
+                                                </div>
+                                            </div>
+
+                                        </Link>
                                     </div>
+                                ))
+                            }
+                        </div>
+                    </div>
 
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-
-        </main>
+                </main>
+            }
+        </>
     )
 }
