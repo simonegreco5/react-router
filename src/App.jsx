eb65d2c8c807564e001c7ab2f797dd929b9190d1
simonegreco5 <simoxey.greco@gmail.com>
2026-05-22 17:32:12 +0200

    - add BudgetContext and integrate budgetMode state in value of BudgetContext.Provider and wrap all BrowserRouter (MILESTONE 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 // sezione import
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useState } from "react"
 import DefaultLayout from "./layout/DefaultLayout"
 import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
 import ProductsPage from "./pages/ProductsPage"
 import ProductPage from "./pages/productPage"
 import ErrorPage from "./pages/ErrorPage"
+import BudgetContext from "./contexts/BudgetContext"
 
 function App() {
+
+  const [budgetMode, setBudgetMode] = useState(false)
 
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/product/:id" element={<ProductPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetContext.Provider>
+
     </>
   )
 }
